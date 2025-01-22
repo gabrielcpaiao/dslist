b8e32747eeed6bb210d72e0da78092d5275dd8ff
Gabriel Paiao <costapaiao@hotmail.com>
2025-01-22 14:09:41 -0300

    Game seed

--- src/main/java/com/trainingDevSuperior/dslist/Entities/Game.java
+++ src/main/java/com/trainingDevSuperior/dslist/Entities/Game.java
@@ -18,10 +18,14 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
+    
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Long getId() {
@@ -56,12 +60,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
@@ -100,13 +104,13 @@ public class Game {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
