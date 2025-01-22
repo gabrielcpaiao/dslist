46980d15ce71f2ad9704cf553a2f2e1028cff2ab
Gabriel Paiao <costapaiao@hotmail.com>
2025-01-22 09:58:37 -0300

    Learning Spring Boot with training

---
+++ src/test/java/com/trainingDevSuperior/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingDevSuperior.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
